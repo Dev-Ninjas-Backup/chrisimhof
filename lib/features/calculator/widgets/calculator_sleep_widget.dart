6262637608e78a38766e7c63b28811b0a8814a88
shahria <shahriasayad9@gmail.com>
2026-04-19 05:43:09 +0600

    custom range slider done

--- lib/features/calculator/widgets/calculator_sleep_widget.dart
+++ lib/features/calculator/widgets/calculator_sleep_widget.dart
@@ -1,4 +1,5 @@
 import 'package:chrisimhof/core/common/controller/time_controller.dart';
+import 'package:chrisimhof/core/common/widgets/custom_range_slider.dart';
 import 'package:chrisimhof/core/common/widgets/time_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -16,6 +17,10 @@ class CalculatorSleepWidget extends StatelessWidget {
     return Column(
       children: [
         TimeWidget(topTitle: 'Wake-up-Time', controller: wakeUpController),
+        SizedBox(height: 16),
+        CustomRangeSlider(headerText: 'Sleep Last Night (h) ', currentValue: 8, onChanged: (double p1) {  }),
+         SizedBox(height: 16),
+        CustomRangeSlider(headerText: 'Sleep Goal (h) ', currentValue: 5, onChanged: (double p1) {  }),
       ],
     );
   }
