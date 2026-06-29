81bb15e5b996a4228e3d4a29e2b95764a0516e0f
tahmidulislamfahim <tahmidulislamfahim@gmail.com>
2026-06-29 16:14:35 +0600

    caffeine card ui change

--- lib/features/dashboard/main_dashboard/widgets/for_you_section.dart
+++ lib/features/dashboard/main_dashboard/widgets/for_you_section.dart
@@ -11,7 +11,7 @@ class ForYouSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.find<RecommendationController>();
+    final controller = Get.put(RecommendationController());
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
